6e1ba4d6bacad2bc39db30b86484c3b0e22d8686
Swifty <Swifty@SWIFTY>
2021-02-11 22:35:57 +0000

    Add .gitignore and .gitattributes.

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using System.Threading.Tasks;
+using Plugins.Vpn.Commands;
+using SharedLibraryCore;
+using SharedLibraryCore.Commands;
+using SharedLibraryCore.Database.Models;
+using SharedLibraryCore.Exceptions;
+using SharedLibraryCore.Interfaces;
+
+namespace vpn_cmds
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "VPN cmds";
+
+        public float Version => Assembly.GetExecutingAssembly().GetName().Version.Major + Assembly.GetExecutingAssembly().GetName().Version.Minor / 10.0f;
+
+        public string Author => "Swifty";
+
+
+        //lol idk what im doing
+        public Task OnEventAsync(GameEvent E, Server S) => Task.CompletedTask;
+
+        public Task OnLoadAsync(IManager manager) => Task.CompletedTask;
+
+        public Task OnTickAsync(Server S) => Task.CompletedTask;
+
+        public Task OnUnloadAsync() => Task.CompletedTask;
+    }
+}
---
+++ VPNCommands.cs
@@ -0,0 +1,67 @@
+﻿using SharedLibraryCore;
+using SharedLibraryCore.Commands;
+using SharedLibraryCore.Configuration;
+using SharedLibraryCore.Database.Models;
+using SharedLibraryCore.Interfaces;
+using System.Threading.Tasks;
+
+namespace Plugins.Vpn.Commands
+{
+    public class AllowVPN_Command : Command
+    {
+        public AllowVPN_Command(CommandConfiguration config, ITranslationLookup translationLookup) : base(config, translationLookup)
+        {
+            Name = "allowvpn";
+            Description = "Add player @id to vpn exception list";
+            Alias = "av";
+            Permission = EFClient.Permission.Trusted;
+            RequiresTarget = true;
+            Arguments = new CommandArgument[]
+            {
+                new CommandArgument()
+                {
+                    Name = "@id or player name",
+                    Required = true
+                }
+            };
+
+
+        }
+
+        public override Task ExecuteAsync(GameEvent E)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+
+    public class RemoveVPN_Command : Command
+    {
+        public RemoveVPN_Command(CommandConfiguration config, ITranslationLookup translationLookup) : base(config, translationLookup)
+        {
+            Name = "removevpn";
+            Description = "Remove player @id from vpn exception list";
+            Alias = "rv";
+            Permission = EFClient.Permission.Trusted;
+            RequiresTarget = true;
+            Arguments = new CommandArgument[]
+            {
+                new CommandArgument()
+                {
+                    Name = "@id or player name",
+                    Required = true
+                }
+            };
+
+
+        }
+
+        public override Task ExecuteAsync(GameEvent E)
+        {
+
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
